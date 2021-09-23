63bc56791d53cfb1b77be01da0090ed67046499f
lalkakrem <lalkakrem@gmail.com>
2021-09-24 00:50:08 +0300

    Изменения кнопок

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text><v-icon left>mdi-chat</v-icon>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
+       <v-btn  text><v-icon left>mdi-account-check</v-icon>Вход</v-btn> 
+       <v-btn  text><v-icon left>mdi-account-circle-outline</v-icon>Регистрация</v-btn>
+	   <v-btn  text><v-icon left>mdi-chat</v-icon>Чат</v-btn> 
+	   <v-btn  text><v-icon left>mdi-ear-hearing</v-icon>Новости</v-btn> 
+	   <v-btn  text><v-icon left>mdi-cat</v-icon>Техподдержка</v-btn> 
     </v-toolbar-items>
   </v-app-bar>
   <v-content>
@@ -16,10 +18,12 @@
   </v-app>
 </template>
 
-
-
 <script>
 export default {
-  
+  data() {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
