8199f4a828a4ba96337a4569767060e43855daf3
lalkakrem <lalkakrem@gmail.com>
2021-09-24 00:30:23 +0300

    Часть 2.2 (со своей иконкой)

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn> 
+       <v-btn  text><v-icon left>mdi-chat</v-icon>Link One</v-btn> 
        <v-btn  text>Link Two</v-btn> 
        <v-btn  text>Link Three</v-btn>
     </v-toolbar-items>
