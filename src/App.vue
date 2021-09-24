c6f5a8c0fb35abe1162f4086515fa8f4fee0cc53
lalkakrem <lalkakrem@gmail.com>
2021-09-24 12:07:47 +0300

    Ветка 2.3

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text><v-icon left>mdi-account-check</v-icon>Вход</v-btn> 
-       <v-btn  text><v-icon left>mdi-account-circle-outline</v-icon>Регистрация</v-btn>
-	   <v-btn  text><v-icon left>mdi-chat</v-icon>Чат</v-btn> 
-	   <v-btn  text><v-icon left>mdi-ear-hearing</v-icon>Новости</v-btn> 
-	   <v-btn  text><v-icon left>mdi-cat</v-icon>Техподдержка</v-btn> 
+       <v-btn text><v-icon left>mdi-account-check</v-icon>Вход</v-btn>
+       <v-btn text><v-icon left>mdi-account-circle-outline</v-icon>Регистрация</v-btn>
+	     <v-btn text><v-icon left>mdi-chat</v-icon>Чат</v-btn>
+	     <v-btn text><v-icon left>mdi-ear-hearing</v-icon>Новости</v-btn>
+	     <v-btn text><v-icon left>mdi-cat</v-icon>Техподдержка</v-btn>
     </v-toolbar-items>
   </v-app-bar>
   <v-content>
