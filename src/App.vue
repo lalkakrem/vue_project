6da9cce886da97a08a7b3ecfc869421a27addd3c
lalkakrem <lalkakrem@gmail.com>
2021-10-07 01:04:43 +0300

    исправленная часть 2.3

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
-  <v-app>
-  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
-  <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn text><v-icon left>mdi-account-check</v-icon>Вход</v-btn>
-       <v-btn text><v-icon left>mdi-account-circle-outline</v-icon>Регистрация</v-btn>
-	     <v-btn text><v-icon left>mdi-chat</v-icon>Чат</v-btn>
-	     <v-btn text><v-icon left>mdi-ear-hearing</v-icon>Новости</v-btn>
-	     <v-btn text><v-icon left>mdi-cat</v-icon>Техподдержка</v-btn>
-    </v-toolbar-items>
-  </v-app-bar>
-  <v-content>
-    <router-view></router-view> 
-  </v-content>
-  </v-app>
+<v-app>
+<v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
+<v-app-bar app dark color="primary">
+<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+<v-spacer></v-spacer>
+<v-toolbar-items class="hidden-sm-and-down">
+       <v-btn  text><v-icon left>mdi-account-check</v-icon>Вход</v-btn> 
+       <v-btn  text><v-icon left>mdi-account-circle-outline</v-icon>Регистрация</v-btn>
+       <v-btn  text><v-icon left>mdi-chat</v-icon>Чат</v-btn>
+       <v-btn  text><v-icon left>mdi-ear-hearing</v-icon>Новости</v-btn>
+       <v-btn  text><v-icon left>mdi-cat</v-icon>Техподдержка</v-btn>
+</v-toolbar-items>
+</v-app-bar>
+<v-content>
+<router-view></router-view>
+</v-content>
+</v-app>
 </template>
-
 <script>
 export default {
-  data() {
-    return {
-      drawer: false
-    }
-  }
+data() {
+return {
+drawer: false
+}
+}
 }
 </script>
