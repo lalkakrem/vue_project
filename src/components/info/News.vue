5e3d67d0a4658563f1ffbedeb8554620ab806416
lalkakrem <lalkakrem@gmail.com>
2021-10-22 10:31:59 +0300

    важные штуки в новостях

--- src/components/info/News.vue
+++ src/components/info/News.vue
@@ -1,6 +1,24 @@
 
 <template>
+  <div>
+  <v-container >
+    <v-layout row >
+      <v-flex >
+        <v-carousel>
+          <v-carousel-item
+              v-for="ad in news"
+              :key="ad.id"
+              :src="ad.src"
+          >
+            <div id="ad-link2">
+              <v-toolbar-text  style="color: #e7d9d9; font-size: 15px; max-width: 20px">{{ad.desc}}</v-toolbar-text>
+            </div>
 
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+  </v-container>
   <v-container fluid fill-height>
     <v-layout align-center justify-center >
       <v-container grid-list-lg>
@@ -35,7 +53,23 @@
       </v-container>
     </v-layout>
   </v-container>
+  </div>
 </template>
+
+<style scoped>
+#ad-link2 {
+  text-align: center;
+  position: absolute;
+  bottom: 50px;
+  left:50%;
+  background: rgba(80, 52, 52, 0.66);
+  transform: translate(-50%,0);
+  padding: 3%;
+  min-width: 100%;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+}
+</style>
 
 <script>
 export default {
@@ -44,42 +78,42 @@ export default {
       news:[
         {
           title:"Хуа Чэн",
-          desc:"Это китаец я не знаю что еще вставлять",
+          desc:"Это китаец я не знаю что еще вставлять его зовут Хуа Чэн",
           promo: true,
           src: "https://pbs.twimg.com/media/EalIX3OXYAAitwa.jpg",
           id:"1"
         },
         {
           title:"Се Лянь",
-          desc:"Это китаец я не знаю что еще вставлять",
+          desc:"Это китаец я не знаю что еще вставлять его зовут Се Лянь или Его Высочество Наследный Принц",
           promo: true,
           src: "http://d.zaix.ru/rnhh.jpg",
           id:"2"
         },
         {
           title:"Вэй У Сянь",
-          desc:"Это китаец я не знаю что еще вставлять",
+          desc:"Это китаец я не знаю что еще вставлять его зовут Вэй У Сянь",
           promo: true,
           src: "http://d.zaix.ru/rnhj.png",
           id:"3"
         },
         {
           title:"Лань Чжань",
-          desc:"Это китаец я не знаю что еще вставлять",
+          desc:"Это китаец я не знаю что еще вставлять его зовут Лань Чжань но вообще-то он Хан Гуан Цзюнь",
           promo: true,
           src: "http://d.zaix.ru/rnhn.png",
           id:"4"
         },
         {
           title:"Цзинь Лин",
-          desc:"Это китаец",
+          desc:"Это китаец он противный но милый МНЕ НУЖНО БОЛЬШЕ ТЕКСТА ДЛЯ ПРОВЕРКИ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ТЕКСТ ",
           promo: true,
           src: "http://d.zaix.ru/rnho.png",
           id:"5"
         },
         {
           title:"Ши Цинсюань",
-          desc:"Это китаец я не знаю что еще вставлять",
+          desc:"Это китаец я не знаю что еще вставлять ПОЧЕМУ ОНО НЕ РАБОТАЕТ",
           promo: true,
           src: "http://d.zaix.ru/rnhq.png",
           id:"6"
@@ -89,42 +123,42 @@ export default {
           desc:"Это китаец я не знаю что еще вставлять",
           promo: true,
           src: "http://d.zaix.ru/rnhn.png",
-          id:"4"
+          id:"7"
         },
         {
           title:"Цзинь Лин",
           desc:"Это китаец",
           promo: true,
           src: "http://d.zaix.ru/rnho.png",
-          id:"5"
+          id:"8"
         },
         {
           title:"Ши Цинсюань",
           desc:"Это китаец я не знаю что еще вставлять",
           promo: true,
           src: "http://d.zaix.ru/rnhq.png",
-          id:"6"
+          id:"9"
         },
         {
           title:"Лань Чжань",
           desc:"Это китаец я не знаю что еще вставлять",
           promo: true,
           src: "http://d.zaix.ru/rnhn.png",
-          id:"4"
+          id:"10"
         },
         {
           title:"Цзинь Лин",
           desc:"Это китаец",
           promo: true,
           src: "http://d.zaix.ru/rnho.png",
-          id:"5"
+          id:"11"
         },
         {
           title:"Ши Цинсюань",
           desc:"Это китаец я не знаю что еще вставлять",
           promo: true,
           src: "http://d.zaix.ru/rnhq.png",
-          id:"6"
+          id:"12"
         },
 
       ]
